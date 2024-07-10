44175b132e775f3079a37a0cba799f43351f887f
saurabh4323 <saurabhiitr01@gmail.com>
2024-07-10 13:03:48 +0530

    de

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Sticky from "./Sticky";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Sticky></Sticky>
     </>
   );
 }
---
+++ src/Sticky.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { SquarePlus } from "lucide-react"; // Import SquarePlus directly
+import "./st.css";
+const Sticky = () => {
+  return (
+    <div className="content">
+      <div className="create">
+        <SquarePlus />
+      </div>
+    </div>
+  );
+};
+
+export default Sticky;
